06b0a24d59534863ae93f2fb5c63e8b9c8a117d1
oott123 <git@public.oott123.com>
2025-03-10 21:34:49 +0800

    chore: remove template modules

--- packages/nest-server/src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { APP_PIPE } from '@nestjs/core'
-import { ZodValidationPipe } from 'nestjs-zod'
-
-@Module({
-  providers: [{ provide: APP_PIPE, useClass: ZodValidationPipe }],
-})
-export class AppModule {}
